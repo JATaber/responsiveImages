41ceed887b149bdd737afa02e704076208d2d09b
James Taber <JATaber@fullsail.edu>
2017-06-18 07:15:36 -0500

    finished icons
    Project completed

--- javascript/content.js
+++ javascript/content.js
@@ -46,8 +46,8 @@ request.onload = function(){
                                     largeImg+' 1080w"' +
                         ' alt="Travel Image">';
                     pictureData += '<div class="picInfo">';
-                    pictureData += '<h2><img class="userIcon" src=" '+userImg+' " alt="user icon"><strong>'+name+'</strong>';
-                    pictureData += '<h3><img class="heartIcon" src=" '+heartImg+' " alt="heart icon"><strong>'+likes+'</strong>';
+                    pictureData += '<h2><img class="userIcon" src=" '+userImg.src+' " alt="user icon"><strong>'+name+'</strong>';
+                    pictureData += '<h3><img class="heartIcon" src=" '+heartImg.src+' " alt="heart icon"><strong>'+likes+'</strong>';
                     pictureData += '</div>';
                     pictureData += '</a>';
                     pictureData += '</article>';
